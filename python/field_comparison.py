b428e3cbd0dbeddbe011af682e0b5b1161dccdbb
sptkl <caobaiyue@gmail.com>
2019-01-02 13:22:26 -0500

    qaqc updates

--- python/field_comparison.py
+++ python/field_comparison.py
@@ -1,24 +1,12 @@
-from pyspark.sql.functions import mean, udf, col
-import pandas as pd
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import mean, udf, col, round
+from pyspark.sql.types import DoubleType
+from pyspark.sql import *
 import time
+import sys
 
-file1 = sys.argv[0]
-file2 = sys.argv[1]
-df1 = spark.read.csv('data/pluto_{}.csv'.format(file1), header=True)
-df2 = spark.read.csv('data/pluto_{}.csv'.format(file2), header=True)
-
-df1 = df1.select([col(A).alias(A+'_1') for A in df1.schema.names])
-df1_names = df1.schema.names
-df2_names = df2.schema.names
-colnames = zip(df1_names, df2_names)
-
-if file1 == '18v11':
-    df = df1.join(df2, df1['bbl_1'] == df2['BBL'])
-elif file2 == '18v11':
-    df = df1.join(df2, df2['bbl_1'] == df1['BBL'])
-else:
-    df = df1.join(df2, df2['BBL_1'] == df1['BBL'])
-
+### Create User Defined functions (udf) for identifying mismatches
 @udf
 def compare(col1,col2):
     if col1!=col2:
@@ -26,9 +14,82 @@ def compare(col1,col2):
     else:
         return 0
 
-spark.sql('set spark.sql.caseSensitive=true')
-for A,B in colnames:
-    df = df.withColumn(A+'%', compare(col(A),col(B)))\
-               .drop(A,B)
+# special access column comparison udf function
+@udf
+def compare_a(col1,col2):
+    try:
+        if abs(col1 - col2) > 10:
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-results = df.select(*[mean(col(A)).alias(A) for A in df.schema.names]).toPandas().to_csv('../output/{}{}.csv'.format(file1, file2))
+if __name__=='__main__':
+    start_time = time.time()
+    sc = SparkContext()
+    spark = SparkSession(sc)
+
+    spark.sql('set spark.sql.caseSensitive=true')
+
+    ### import data and prepare dataframe ###
+    pluto_1 = sys.argv[0]
+    pluto_2 = sys.argv[1]
+    output_csv = sys.argv[2]
+
+    ### Float type columns
+    double_columns = ['bldgarea', 'facilfar','residfar', 'commfar',
+                    'numbldgs', 'numfloors', 'bldgdepth', 'bldgfront',
+                    'lotdepth', 'lotfront', 'exempttot', 'exemptland',
+                    'assessland', 'assesstot', 'builtfar']
+
+    ### Assess related columns --> apply compare_a
+    access_cols = ['exemptland', 'exempttot', 'assessland', 'assesstot']
+
+    df1 = spark.read.csv(pluto_1, header=True)
+    df2 = spark.read.csv(pluto_2, header=True)
+
+    for A in df1.columns:
+            df1 = df1.withColumnRenamed(A, A.lower())
+
+    for A in df2.columns:
+            df2 = df2.withColumnRenamed(A, A.lower())
+
+    # dbutils.fs.rm('data/df1.parquet', True)
+    df1.write.parquet('data/df1.parquet')
+    # df1 = df1.select([col(A).(A.lower()) for A in df1.schema.names])
+    # df2 = df2.select([col(A).alias(A.lower()) for A in df2.schema.names])
+
+    ### Select column intersections
+    # print(df1.columns)
+    # print('A', df2.columns)
+    # print('B', df1.columns)
+
+    # df11 = df1.select(df1.columns)
+    # df22 = df2.select(df1.columns)
+
+    ### Column type conversions
+    # for A in double_columns:
+    #     df1 = df1.withColumn(A, round(col(A).cast(DoubleType()), 2))
+    #     df2 = df2.withColumn(A, round(col(A).cast(DoubleType()), 2))
+
+    ### Add column name flag to distinguish two versions
+    # df1 = df1.select([col(A).alias(A+'_1') for A in df1.schema.names])
+    # colnames = zip(df1.columns, df2.columns)
+
+    ### merging two dataframes on bbl (inner join)
+    # df = df2.join(df1, df2['bbl'] == df1['bbl_1'], 'inner')
+    #
+    # ### Apply udf for field by field comparison
+    # for A,B in colnames:
+    #     if B in access_cols:
+    #         df = df.withColumn(B+'%', compare_a(col(A),col(B)))\
+    #             .drop(A,B)
+    #     else:
+    #         df = df.withColumn(B+'%', compare(col(A),col(B)))\
+    #                 .drop(A,B)
+    #
+    # ### Reduce table by taking means of each column and export result table to csv
+    # results = df.select(*[mean(col(A)).alias(A) for A in df.columns]).write.csv(output_csv)
+    # elapsed_time = time.time() - start_time
+    # print('This job took {}s to execute'.format(elapsed_time))
